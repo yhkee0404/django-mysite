6a00936a12574fa186fc778f4400f349132acbf1
yhkee0404 <yhkee0404@gmail.com>
2022-12-15 15:09:25 +0900

    Include snippets.urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,6 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
